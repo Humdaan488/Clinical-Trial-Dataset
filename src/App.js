1b7793d55728cfb0dc31a0682c677fdf4295c87d
Humdaan Javed <77771083+Humdaan488@users.noreply.github.com>
2024-03-06 11:31:52 -0500

    Adding main project files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { MantineProvider } from '@mantine/core';
+import DisplayTable from './DisplayTable';
+import './App.css';
+import vialLogo from './viallogo-1.png';
+
+function App() {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <div className="App">
+        <img src={vialLogo} alt="Vial Logo" className="top-left-logo" />
+        <h1 className="center-text">Clinical Trial Dataset</h1>
+        <div className="vision-statement center-text">OUR VISION IS TO EMPOWER SCIENTISTS TO CURE ALL HUMAN DISEASE.</div>
+        <DisplayTable />
+      </div>
+    </MantineProvider>
+  );
+}
+
+export default App;
---
+++ src/DisplayTable.js
@@ -0,0 +1,82 @@
+// src/DisplayTable.js : Main component to display Table and handle filtering/sorting logic
+
+import { useMemo } from 'react';
+import { MantineReactTable, useMantineReactTable } from 'mantine-react-table';
+import { Loader } from '@mantine/core';
+import useTableData from './TableData'
+
+// List of options for dropdown of Gender Attribute
+const genderList = [ "Male",  "Female"];
+
+const DisplayTable = () => {
+  const { tableSubjects, loading } = useTableData();
+
+  const columns = useMemo(
+    () => [
+        // Setting up 6 columns and their filter mechanism using subjects data consumed from endpoint
+        {
+            accessorKey: 'name',
+            header: 'Name',
+            filterVariant: 'text',
+        },
+        {
+            accessorKey: 'age',
+            header: 'Age',
+            filterVariant: 'range',
+            filterFn: 'between',
+            size: 80,
+        },
+        {
+            accessorKey: 'gender',
+            header: 'Gender',
+            filterVariant: 'select',
+            mantineFilterSelectProps: {
+              data: genderList,
+            },
+        },
+        {
+            // Accessor function to convert string to date for filtering
+            accessorFn: (originalRow) => new Date(originalRow.diagnosisDate), 
+            id: 'diagnosisDate',
+            header: 'Diagnosis Date',
+            filterVariant: 'date-range',
+            // Converting value back to string for display
+            Cell: ({ cell }) => cell.getValue().toLocaleDateString(),
+        },
+        {
+            accessorFn: (originalRow) => (originalRow.isActive ? 'true' : 'false'), 
+            id: 'isActive',
+            header: 'Status',
+            filterVariant: 'checkbox',
+            Cell: ({ cell }) =>
+            cell.getValue() === 'true' ? 'Active' : 'Inactive',
+            size: 220,
+      },
+      {
+            accessorKey: 'id',
+            header: 'Unique ID',
+            filterVariant: 'text',
+      }, 
+    ],
+    [],
+  );
+
+  const tableInstance = useMantineReactTable({
+    columns,
+    data: tableSubjects,
+    initialState: { showColumnFilters: true },
+    enableRowSelection: true,
+    enableColumnOrdering: true,
+    enablePinning: true,
+    mantineSelectCheckboxProps: {
+      color: 'teal',
+   },
+  });
+
+  // User experience - returns loader component if data is still loading
+  if (loading) return <Loader size="xl" color="blue" />;
+  
+  return <MantineReactTable table={tableInstance} />;
+};
+
+export default DisplayTable;
